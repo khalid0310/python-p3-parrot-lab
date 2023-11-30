cb9b9207a19cdd634a0c918a38b133c6baa0da03
khalid <khalid.mohamed@moringaschool.com>
2023-11-30 09:07:29 +0300

    complete

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,13 @@
-def parrot():
-    pass
+
+def parrot(message="Squawk!"):
+    """
+    Accepts a string as an argument, prints it, and returns it.
+
+    Parameters:
+    - message (str): The message to be printed and returned.
+
+    Returns:
+    - str: The same message that was passed as an argument.
+    """
+    print(message)
+    return message
